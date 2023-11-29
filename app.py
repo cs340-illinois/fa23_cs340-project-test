902be8dec9281f3951f4142ff82a22d366828318
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-11-28 23:35:10 -0600

    format whitespace

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 def init():
     dotenv.load_dotenv()
+
 
 status = 'accept'
 xdim = 10
@@ -29,23 +31,25 @@ currentClientID = 'none'
 currentURL = 'none'
 currentAuthor = 'none'
 
+
 @app.route('/getState', methods=['GET'])
 def GET_state():
     state = jsonify({
-        'status':status,
-        'xdim':xdim,
-        'ydim':ydim,
-        'tilesize':tilesize,
-        'xloc':xloc,
-        'yloc':yloc,
-        'voteToken':voteToken,
-        'approved':approved,
-        'currentAuthToken':currentAuthToken,
-        'currentAuthor':currentAuthor,
-        'currentClientID':currentClientID,
-        'currentURL':currentURL
+        'status': status,
+        'xdim': xdim,
+        'ydim': ydim,
+        'tilesize': tilesize,
+        'xloc': xloc,
+        'yloc': yloc,
+        'voteToken': voteToken,
+        'approved': approved,
+        'currentAuthToken': currentAuthToken,
+        'currentAuthor': currentAuthor,
+        'currentClientID': currentClientID,
+        'currentURL': currentURL
     })
     return state, 200
+
 
 @app.route('/accept', methods=['POST'])
 def POST_accept():
@@ -53,11 +57,13 @@ def POST_accept():
     status = 'accept'
     return "OK", 200
 
+
 @app.route('/reject', methods=['POST'])
 def POST_reject():
     global status
     status = 'reject'
     return "OK", 200
+
 
 @app.route('/registerImage/<clientID>', methods=["POST"])
 def POST_registerImage(clientID):
@@ -65,7 +71,8 @@ def POST_registerImage(clientID):
     if (status == 'accept'):
         return "OK", 200
     else
-        return "Wrong Dimension", 500
+    return "Wrong Dimension", 500
+
 
 @app.route('/registerClient/<clientID>', methods=["PUT"])
 def PUT_registerClient(clientID):
@@ -84,29 +91,32 @@ def PUT_registerClient(clientID):
     currentAuthToken = data["token"]
     global currentAuthor
     currentAuthor = data["author"]
-    
+
     # otherwise return 200 "success" and json with
     # xdim : int // in tiles
     # ydim : int // in tiles
     # tilesize : int
     canvasInfo = jsonify({
-        'xdim':xdim,
-        'ydim':ydim,
-        'tilesize':tilesize
+        'xdim': xdim,
+        'ydim': ydim,
+        'tilesize': tilesize
     })
     return canvasInfo, 200
+
 
 @app.route("/registeredTest", methods=["GET"])
 def GET_registeredTest():
 
     state = jsonify({
-        'xloc':xloc,
-        'yloc':yloc,
-        'voteToken':voteToken,
-        'approved':approved,
-        'authToken':currentAuthToken,
+        'xloc': xloc,
+        'yloc': yloc,
+        'voteToken': voteToken,
+        'approved': approved,
+        'authToken': currentAuthToken,
     })
 
-    response = requests.put(f'{currentURL}/registered', data=json.dumps(state), headers={'Content-Type': 'application/json'})
+    response = requests.put(f'{currentURL}/registered', data=json.dumps(
+        state), headers={'Content-Type': 'application/json'})
 
     return response
+
