0f340336297b2b24bcb3e014adf46309e5d1b1ef
Kaiwen-Ren <92759144+Kaiwen-Ren@users.noreply.github.com>
2023-12-06 23:57:27 -0600

    Kaiwen Ren week 2 server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify, render_template, request, send_file
+from flask import Flask, jsonify, render_template, request, send_file, Response
 import requests
 import dotenv
 import os
@@ -9,63 +9,152 @@ app = Flask(__name__)
 status = 'accept'
 xdim = 10
 ydim = 10
-tilesize = 5
-xloc = 0
-yloc = 0
+tilesize = 10
+xloc = 5
+yloc = 5
 voteToken = '45046605-4ef3-4dfc-84ed-cfafade8a2db'
 
+# For simple testing just use one client
 approved = 'true'
 currentAuthToken = 'none'
 currentClientID = 'none'
 currentURL = 'none'
 currentAuthor = 'none'
+seq = 0
 
 UPLOAD_FOLDER = "uploads"
 
 image_requests = []
-client_image_mapping = {} 
+vote_score = {voteToken: 0} # voteToken: score
+tile_map = {voteToken: (xloc, yloc)}
 
 def init():
     dotenv.load_dotenv()
 
 @app.route('/')
 def index():
-    global file
     return render_template('index.html')
 
-@app.route('/getClientData', methods=['GET'])
-def GET_client_data():
-    client_data = []
-    for clientID, images in client_image_mapping.items():
-        client_data.append({
-            'clientID': clientID,
-            'imageURLs': [f'/getImage/{clientID}/{image}' for image in images]
-        })
-    return jsonify(client_data), 200
+# test GET /image and GET /tile
+TILE_SERVER_HOST = 'http://localhost'
+TILE_SERVER_PORT = '34000' # change this based on your tile server port
 
-@app.route('/getState', methods=['GET'])
-def GET_state():
-    state = jsonify({
-        'status': status,
-        'xdim': xdim,
-        'ydim': ydim,
-        'tilesize': tilesize,
-        'xloc': xloc,
-        'yloc': yloc,
-        'voteToken': voteToken,
-        'approved': approved,
-        'currentAuthToken': currentAuthToken,
-        'currentAuthor': currentAuthor,
-        'currentClientID': currentClientID,
-        'currentURL': currentURL
+@app.route('/display-image')
+def display_image():
+    image_response = requests.get(f'{TILE_SERVER_HOST}:{TILE_SERVER_PORT}/image')
+    if image_response.status_code == 200:
+        return Response(image_response.content, mimetype='image/png')
+    else:
+        return "Image not available", image_response.status_code
+
+@app.route('/display-tile')
+def display_tile():
+    tile_response = requests.get(f'{TILE_SERVER_HOST}:{TILE_SERVER_PORT}/tile')
+    if tile_response.status_code == 200:
+        return Response(tile_response.content, mimetype='image/png')
+    else:
+        return "Tile not available", tile_response.status_code
+
+# test vote
+
+# You need to add this function to your tile server
+# and modify the canvas url to yours
+
+# @app.route('/submit-vote', methods=['POST'])
+# def submit_vote():
+#     # Extracting xloc and yloc from the request body
+#     data = request.get_json()
+#     x = data['xloc']
+#     y = data['yloc']
+    
+#     voteToken = token['voteToken']
+#     canvas_payload = {
+#         "voteToken": voteToken,
+#         "xloc": x,
+#         "yloc": y
+#     }
+
+#     # URL of the canvas server's voting endpoint
+#     canvas_url = 'http://127.0.0.1:5000/vote/your_netid'
+#     print("start to PUT")
+#     # Sending the POST request to the canvas server
+#     response = requests.put(canvas_url, json=canvas_payload)
+    
+#     # Handle the response
+#     if response.status_code == 200:
+#         print("message", "Vote submitted successfully",200)
+#         return jsonify(canvas_payload), 200
+#     else:
+#         print("error", "Failed to submit vote",response)
+#         return jsonify({"error": "Failed to submit vote"}), response.status_code
+
+@app.route('/submit-vote', methods=['POST'])
+def proxy_submit_vote():
+    data = request.get_json()
+    response = requests.post('http://localhost:34000/submit-vote', json=data)
+    return jsonify(response.json()), response.status_code
+
+@app.route('/test-GETvotes', methods=['GET'])
+def test_vote2():
+    url = currentURL + '/votes'
+    response = requests.get(url)
+    print(jsonify(response.text), response.status_code)
+    return jsonify(response.text), response.status_code
+
+
+@app.route('/test-PUTvotes', methods=['GET'])
+def test_vote1():
+    response = requests.put(f'{TILE_SERVER_HOST}:{TILE_SERVER_PORT}/votes', json={
+        'token': currentAuthToken,
+        'votes': vote_score[voteToken],
+        'seq': seq,     
     })
-    return state, 200
+    return jsonify(response.text), response.status_code
 
+@app.route('/vote/<clientID>', methods=["PUT"])
+def putVotes(clientID):
+    data = request.get_json()
+    
+    # Validating if the necessary data is present
+    if not data or 'voteToken' not in data or 'xloc' not in data or 'yloc' not in data:
+        return jsonify({"error": "Missing data"}), 400
+
+    # Extracting the voteToken, xloc, and yloc from the request body
+    vote_token = data['voteToken']
+    dx = data['xloc']
+    dy = data['yloc']
+    dx = int(dx)
+    dy = int(dy)
+
+    if vote_token != voteToken:
+        return jsonify({"error": "Invalid vote token"}), 401
+    
+    # update sequence
+    global seq
+    seq += 1
+    
+    # update vote score
+    for token in tile_map:
+        x, y = tile_map[token]
+        if dx == x and dy == y: 
+            print("your are voted!")
+            vote_score[token] += 1
+
+    print("You vote to tile: ", dx, dy)
+    print("Your tile position: ", tile_map[vote_token])
+    print("Your vote score: ", vote_score[vote_token])
+
+    return jsonify(data), 200
+
+
+
+# Week 1 stuff
 @app.route('/accept', methods=['POST'])
 def POST_accept():
     global status
     status = 'accept'
     return "OK", 200
+
 
 @app.route('/reject', methods=['POST'])
 def POST_reject():
@@ -73,10 +162,12 @@ def POST_reject():
     status = 'reject'
     return "OK", 200
 
+
 @app.route('/registerImage/<clientID>', methods=["POST"])
 def POST_registerImage(clientID):
     if 'file' not in request.files:
         return "", 400
+
     file = request.files['file']
 
     if not file or not file.filename or file.filename == '':
@@ -85,12 +176,6 @@ def POST_registerImage(clientID):
     os.makedirs(UPLOAD_FOLDER + "/" + clientID, exist_ok=True)
     file.save(UPLOAD_FOLDER + "/" + clientID + "/" + file.filename)
     print("file saved")
-
-    if clientID in client_image_mapping:
-        client_image_mapping[clientID].append(file.filename)
-    else:
-        client_image_mapping[clientID] = [file.filename]
-
     return "", 200
 
 
@@ -118,7 +203,6 @@ def PUT_registerClient(clientID):
         'ydim': ydim,
         'tilesize': tilesize
     })
-
     return canvasInfo, 200
 
 
@@ -133,8 +217,8 @@ def GET_registeredTest():
         'authToken': currentAuthToken,
     })
 
-    return response.text, response.status_code
-
-@app.route('/getImage/<clientID>/<imageFileName>', methods=["GET"])
-def GET_image(clientID, imageFileName):
-    return send_file(f"uploads/{clientID}/{imageFileName}", mimetype='image/png') 
+    print(jsonify(response.text), response.status_code)
+    return jsonify(response.text), response.status_code
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
